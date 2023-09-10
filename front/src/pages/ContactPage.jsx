6942bf6868d51baf0255124d7088be54c400eed8
ThaoNGUYEN1411 <phuongthaolyon@gmail.com>
2023-09-10 17:43:11 +0200

    créer formulaire page contact

--- front/src/pages/ContactPage.jsx
+++ front/src/pages/ContactPage.jsx
@@ -3,15 +3,36 @@ import styles from "./ContactPage.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faAddressBook,
+	faChevronRight,
 	faEnvelope,
 	faPhone,
 } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
+import Button from "../components/Button/Button";
+import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 
 const ContactPage = () => {
+	const {
+		handleSubmit,
+		watch,
+		register,
+		formState: { isValid, isSubmitted, submitCount, errors },
+	} = useForm();
+	const onSubmit = (values) => {
+		console.log(values);
+	};
+
+	useEffect(() => {
+		// const subscription = watch(observer => console.log(observer));
+		const subscription = watch((observer) => console.log(observer));
+
+		return () => subscription.unsubscribe();
+	}, [watch]);
+
 	return (
-		<div className={cx("grid", "wide")}>
+		<div className={cx("grid", "wide", "wrapper")}>
 			<section className={cx("margin-bloc")}>
 				<h1 className={cx("title-block")}>Nous contacter</h1>
 				<div className={cx("contact", "row")}>
@@ -44,46 +65,93 @@ const ContactPage = () => {
 					</div>
 				</div>
 			</section>
-			<section>
-				<h2>Envoyez nous un message</h2>
-				<div className={cx("contact-bloc", "row")}>
-					<div className={cx("l-12", "m-12", "c12")}>
-						<input
-							className={cx("contact-input")}
-							type="text"
-							placeholder="Choisissez un sujet"
-						/>
-					</div>
-					<div className={cx("l-12", "m-12", "c12")}>
-						<input
-							className={cx("contact-input")}
-							type="text"
-							placeholder="Votre nom"
-						/>
-					</div>
-					<div className={cx("l-12", "m-12", "c12")}>
-						<input
-							className={cx("contact-input")}
-							type="text"
-							placeholder="Votre prénom"
-						/>
-					</div>
-					<div className={cx("l-12", "m-12", "c12")}>
-						<input
-							className={cx("contact-input")}
-							type="text"
-							placeholder="Votre e-mail"
-						/>
-					</div>
+			<section className={cx("contact-form")}>
+				<h2 className={cx("text-center", "title-form")}>
+					Envoyez nous un message
+				</h2>
 
-					<div className={cx("l-12", "m-12", "c12")}>
-						<input
-							className={cx("contact-input")}
-							type="text"
-							placeholder="Ecrire votre message ici... "
-						/>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className={cx("contact-bloc", "row")}>
+						<p className={cx("contact-info", "l-12", "m-12", "c-12")}>
+							<label className={cx("label")}>Votre sujet</label>
+							<select
+								{...register("sujet", { required: "Un message est requis" })}
+								className={cx("contact-input")}
+							>
+								<option value="Choisissez un sujet">Choisissez un sujet</option>
+								<option value="Suggérer une marque">Suggérer une marque</option>
+								<option value="Publicité">Publicité</option>
+								<option value="Signaler un problème de connexion">
+									Signaler un problème de connexion
+								</option>
+								<option value="Signaler un problème d'inscription">
+									Signaler un problème d'inscription
+								</option>
+								<option value="Signaler un contenu">Signaler un contenu</option>
+								<option value="Suggérer une amélioration">
+									Suggérer une amélioration
+								</option>
+								<option value="Autre">Autre</option>
+							</select>
+							{errors.sujet && (
+								<span className="error-message">{errors.sujet.message}</span>
+							)}
+						</p>
+						<div className={cx("row", "name")}>
+							<p className={cx("contact-info", "l-6", "m-6", "c-12")}>
+								<label className={cx("label", "label-name")}>Votre nom</label>
+								<input
+									className={cx("lastname")}
+									type="text"
+									placeholder="Votre nom"
+									{...register("lastname", {
+										required: "Votre nom est requis",
+									})}
+								/>
+								<span>{errors.lastname?.message}</span>
+							</p>
+							<p className={cx("contact-info", "l-6", "m-6", "c-12")}>
+								<label className={cx("label", "label-name")}>
+									Votre prénom
+								</label>
+								<input
+									className={cx("firstname")}
+									type="text"
+									placeholder="Votre prénom"
+									{...register("firstname")}
+								/>
+							</p>
+						</div>
+						<p className={cx("contact-info", "l-12", "m-12", "c-12")}>
+							<label className={cx("label")}>Votre e-mail</label>
+							<input
+								className={cx("contact-input")}
+								type="email"
+								placeholder="Votre e-mail"
+								{...register("email", {
+									required: "Email est requis",
+								})}
+							/>
+							<span>{errors.email?.message}</span>
+						</p>
+						<p className={cx("contact-info", "l-12", "m-12", "c-12")}>
+							<label className={cx("label")}>Votre message : </label>
+							<textarea
+								{...register("message", { required: "Un message est requis" })}
+								className={cx("contact-input")}
+							/>
+							<span>{errors.message?.message}</span>
+						</p>
 					</div>
-				</div>
+					<div className={cx("text-center")}>
+						<Button primary>
+							Envoyer
+							<span>
+								<FontAwesomeIcon icon={faChevronRight} />
+							</span>
+						</Button>
+					</div>
+				</form>
 			</section>
 		</div>
 	);
